21791823dc46d745292c39daa0c9ccbdd3128727
Andres Velasco <velasco.andrs@gmail.com>
2024-07-25 08:56:04 -0500

    fix(crons): change cron notation

--- src/consts.rs
+++ src/consts.rs
@@ -2,10 +2,10 @@
     Define events cronjobs here
 */
 // Each saturdays at 17pm UTC  -> 12pm in UTC-5
-pub const WALLET_REFILL_EVENT: &str = "0 17 * * 6";
+pub const WALLET_REFILL_EVENT: &str = "0 17 * * sat";
 // Each day at 3am UTC -> 10pm in UTC-5
 pub const GREETING_GOOD_NIGHT_EVENT: &str = "0 3 * * *";
 // Each day at 13pm UTC -> 8am in UTC-5
 pub const GREETING_GOOD_MORNING_EVENT: &str = "0 13 * * *";
 // Each day at 10pm UTC -> 6am in UTC-5
-pub const ENGLISH_DAY_EVENT: &str = "0 10 * * 5";
+pub const ENGLISH_DAY_EVENT: &str = "0 10 * * fri";
