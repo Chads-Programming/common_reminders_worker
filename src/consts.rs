e446ff4920161399da1566ad7cf6c7f370f4d22e
Andres Velasco <velasco.andrs@gmail.com>
2024-07-29 23:36:38 -0500

    fix(reminders): english day cronjobs

--- src/consts.rs
+++ src/consts.rs
@@ -7,7 +7,5 @@ pub const WALLET_REFILL_EVENT: &str = "0 17 * * sat";
 pub const GREETING_GOOD_NIGHT_EVENT: &str = "0 3 * * *";
 // Each day at 13pm UTC -> 8am in UTC-5
 pub const GREETING_GOOD_MORNING_EVENT: &str = "0 13 * * *";
-// Each friday at 10pm UTC -> 6am in UTC-5
-pub const ENGLISH_DAY_EVENT_FRI: &str = "0 10 * * fri";
-// Each wednesday at 10pm UTC -> 6am in UTC-5
-pub const ENGLISH_DAY_EVENT_WED: &str = "0 10 * * wed";
+// Each wednesday, friday at 10pm UTC -> 6am in UTC-5
+pub const ENGLISH_DAY_EVENT: &str = "0 10 * * wed,fri";
